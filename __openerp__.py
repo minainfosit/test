7cc0a9d00ff5242a0c0c013beecea4418f66d0cb
ubuntu <ubuntu@ubuntu-HP-Pavilion-dv7-Notebook-PC>
2015-05-11 14:33:13 +0100

    1 commit

---
+++ __openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'test',
+    'version': '0.1',
+    'category': 'Generic Modules/Others',
+    'description': """ Test creation module new api Odoo v8
+    """,
+    'author': 'PMA',
+    'depends': ['base'],
+    'data': ['test_view.xml'],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields
+
+class author(models.Model):
+    _name = 'test.author'
+    _rec_name = 'lastname'
+    firstname = fields.Char('FirstName', required=True)
+    lastname = fields.Char('LastName', required=True)
+    cin = fields.Char('cin', required=True)
+    book_ids = fields.One2many('test.book','author_id','Books')
+ 
+class book(models.Model):
+    _name = 'test.book'
+    title = fields.Char('Title', required=True)
+    genre = fields.Char('Genre', required=True)
+    author_id = fields.Many2one('test.author','Author',ondelete='cascade')
