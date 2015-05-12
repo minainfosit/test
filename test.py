e4112d5ea4528bc1dd21f202c05a7b884e2ae6ec
ubuntu <ubuntu@ubuntu-HP-Pavilion-dv7-Notebook-PC>
2015-05-12 16:10:56 +0100

    mon 1ere commit

--- test.py
+++ test.py
@@ -8,9 +8,17 @@ class author(models.Model):
     lastname = fields.Char('LastName', required=True)
     cin = fields.Char('cin', required=True)
     book_ids = fields.One2many('test.book','author_id','Books')
+    ville_id = fields.Many2one('test.ville' , 'ville')
+
  
 class book(models.Model):
     _name = 'test.book'
     title = fields.Char('Title', required=True)
     genre = fields.Char('Genre', required=True)
     author_id = fields.Many2one('test.author','Author',ondelete='cascade')
+
+class ville(models.Model):
+    _name = 'test.ville'
+    name = fields.Char('ville', required=True)
+    
+     
